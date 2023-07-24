40348148f55af636c396507af23e810c7ed862e4
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-24 06:41:09 -0700

    2nd commit

--- main.h
+++ main.h
@@ -2,9 +2,10 @@
 #define MY_HEADER_FILE
 
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
 #include <stdlib.h>
-
 
 int _printchar(int c);
 int _printstring(char *s);
